1efff77ce3b2b69d9044687356b3aab4d1856cf5
Kirill <zarj09@gmail.com>
2021-04-21 22:48:52 +0300

    make it bette

--- parfumer/settings.py
+++ parfumer/settings.py
@@ -153,7 +153,7 @@ STATICFILES_DIRS = (
 )
 CKEDITOR_UPLOAD_PATH = "uploads/"
 
-db_from_env = dj-database-url.config()
+db_from_env = dj_database_url.config()
 DATABASE['default'].update(db_from_env)
 
 # +++++++++  статика амазон +++++++++++++++++ #
